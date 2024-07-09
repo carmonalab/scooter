fdb7fdc8d2b38e8b1118faccb00c49cc4471f94b
halterc <chalterbio@gmail.com>
2024-07-09 10:38:41 +0200

    Separate from HiTME

--- R/utils.R
+++ R/utils.R
@@ -13,158 +13,6 @@ methods::setClass(Class = "HiT",
 
 
 
-# ProjecTILs on multiple reference maps #############################################################
-
-ProjecTILs.classifier.multi <- function(object,
-                                        ref.maps,
-                                        bparam = NULL,
-                                        layer1_link = "CellOntology_ID") {
-
-  # count how many cells are found by scGate for each map reference
-  if (layer1_link %in% names(object@meta.data)) {
-    filter.cells <- F
-    map.celltypes <- lapply(ref.maps,
-                            function(x) {
-                              ct <- x@misc$layer1_link
-                              nrow(object@meta.data[object@meta.data[[layer1_link]] %in% ct,])
-                            })
-
-    present <- names(ref.maps[map.celltypes > 1])
-    no.present <- names(ref.maps)[!names(ref.maps) %in% present]
-
-    if (length(present) == 0) {
-      message(paste("No cells linked to reference maps found by", layer1_link,
-                    ".\nNot running Projectils"))
-      run <- FALSE
-    } else {
-      run <- TRUE
-      message("Performing Projectils classification for ",
-              paste(present, collapse = ", "))
-
-      if (length(ref.maps) != length(present)) {
-        message("Not doing mapping for ",
-                paste(no.present, collapse = ", ") )
-        ref.maps <- ref.maps[present]
-      }
-    }
-
-  } else {
-    filter.cells <- TRUE
-
-    message("Not scGate classification found in this object\n",
-            "Running default scGate (or layer1 classification) filtering within Projectils)")
-    run <- TRUE
-  }
-
-  if (run) {
-    suppressWarnings(require(ProjecTILs))
-
-    functional.clusters <-
-      BiocParallel::bplapply(
-        X = names(ref.maps),
-        BPPARAM = bparam,
-        function(m) {
-          if (filter.cells) {
-            # don't subset if no scGate gating is found in metadata, instead run scGate within projectils
-            subset.object <- object
-          } else {
-            map.celltype <- ref.maps[[m]]@misc$layer1_link
-            subset.object <- object[,object@meta.data[[layer1_link]] %in% map.celltype]
-
-          }
-
-          if (ncol(subset.object)>0) {
-            message("\nRunning Projectils for ", m, " reference")
-            # it is mandatory to make run in serial (ncores = 1 and BPPARAM SerialParam)
-            subset.object.pred <- ProjecTILs:::classifier.singleobject(subset.object,
-                                                                       ref = ref.maps[[m]],
-                                                                       filter.cells = filter.cells,
-                                                                       ncores = 1)
-
-            return(subset.object.pred)
-
-          } else {
-            message("Not Running Projectils classifier for reference map",
-                    paste(map.celltype, collapse = ", "), ". No cells found")
-          }
-        }
-      )
-
-
-    if (!all(unlist(lapply(functional.clusters, function(x) {is.null(x)})))) {
-      functional.clusters <- data.table::rbindlist(lapply(functional.clusters,
-                                                          data.table::setDT,
-                                                          keep.rownames = TRUE)) %>%
-        #remove duplicated row.names (classified by different ref.maps)
-        dplyr::filter(!duplicated(rn)) %>%
-        tibble::column_to_rownames("rn")
-
-      # remove already present functional.cluster columns
-      rm <- grep("^functional.cluster",
-                 names(object@meta.data))
-      if (length(rm) > 0) {
-        object@meta.data <- object@meta.data[, -rm, drop = F]
-      }
-      object@meta.data <- merge(object@meta.data,
-                                functional.clusters,
-                                by = 0,
-                                all.x = T) %>%
-        tibble::column_to_rownames("Row.names")
-
-      # save names of reference maps run (will be lost if objects remerged)
-      object@misc[["layer2_param"]][["functional.cluster"]][["References_executed"]] <- names(ref.maps)
-
-    } else {
-      object@meta.data <- object@meta.data %>%
-        dplyr::mutate(functional.cluster = NA,
-                      functional.cluster.conf = NA)
-    }
-  }
-  return(object)
-}
-
-
-
-
-match_dictionary <- function(cell_type,
-                             dictionary = NULL) {
-
-  if (is.null(dictionary)) {
-    dict <- c("^T|CTL" = "T cell",
-              "^B|B$" = "B cell",
-              "^NK" = "NK",
-              "Mono" = "Monocyte-like",
-              "^Fibro" = "Fibroblast",
-              "^Malig|cancer|tumo[u]r" = "Malignant",
-              "Myeloid" = "Myeloid",
-              "HSC" = "HSC",
-              "DC|Dendri" = "DC",
-              "^Epi|^EC" = "Epithelial",
-              "Mast" = "Mastocyte",
-              "Macroph" = "Macrophage",
-              "^Endo" = "Endothelial",
-              "Neutro" = "Neutrophil",
-              "Strom" = "Stromal")
-  }
-
-  for (keyword in names(dict)) {
-    if (grepl(keyword, cell_type, ignore.case = TRUE)) {
-      cell_type <- dict[[keyword]]
-      break
-    }
-  }
-  return(cell_type)
-}
-
-
-
-StandardizeCellNames <- function(cell.names, dictionary = NULL) {
-  standarized_cellnames <- sapply(cell.names, match_dictionary, dictionary)
-  standarized_cellnames <- unname(standarized_cellnames)
-
-  return(standarized_cellnames)
-}
-
 
 
 set_parallel_params <- function(ncores,
@@ -282,7 +130,6 @@ preproc_pseudobulk <- function (matrix,
                                 metadata,
                                 cluster.by,
                                 nVarGenes = 500,
-                                gene.filter = "HVG",
                                 black.list = NULL) {
 
   suppressMessages({
@@ -291,7 +138,6 @@ preproc_pseudobulk <- function (matrix,
                                  metadata = metadata,
                                  cluster.by = cluster.by,
                                  nVarGenes = nVarGenes,
-                                 gene.filter = gene.filter,
                                  black.list = black.list)
     })
   })
@@ -307,7 +153,6 @@ DESeq2.normalize <- function(matrix,
                              metadata,
                              cluster.by,
                              nVarGenes = 500,
-                             gene.filter = "HVG",
                              black.list = NULL) {
 
   # Get black list
@@ -333,18 +178,10 @@ DESeq2.normalize <- function(matrix,
   # Remove black listed genes from the matrix
   data <- data[!row.names(data) %in% black.list,]
 
-  # filter genes accordingly
-  if (gene.filter == "HVG") {
-    # get top variable genes
-    rv <- MatrixGenerics::rowVars(data)
-    select <- order(rv, decreasing=TRUE)[seq_len(min(nVarGenes, length(rv)))]
-    select <- row.names(data)[select]
-
-  } else if (gene.filter == "default_filter") {
-    # get predetermined list of genes
-    utils::data("GO_accession_default")
-    select <- GO_default
-  }
+  # get top variable genes
+  rv <- MatrixGenerics::rowVars(data)
+  select <- order(rv, decreasing=TRUE)[seq_len(min(nVarGenes, length(rv)))]
+  select <- row.names(data)[select]
 
   data <- data[select[select %in% row.names(data)],]
 
